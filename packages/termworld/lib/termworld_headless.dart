8a6103ebef1a047c9d410c0684e8acbfcf61815c
winetree94 <me@winetree94.com>
2026-08-09 05:08:56 +0900

    port xterm character joiner service

--- packages/termworld/lib/termworld_headless.dart
+++ packages/termworld/lib/termworld_headless.dart
@@ -3,6 +3,7 @@ library;
 
 export 'src/core/addon.dart';
 export 'src/core/buffer.dart';
+export 'src/core/character_joiner_service.dart';
 export 'src/core/decoration_service.dart';
 export 'src/core/disposable.dart';
 export 'src/core/event.dart';
