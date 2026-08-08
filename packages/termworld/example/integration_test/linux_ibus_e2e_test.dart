45ce59da8d3448ca6c6f5b5a2972398e4fe0f1c6
winetree94 <me@winetree94.com>
2026-08-09 08:35:31 +0900

    Send complete IBus Hangul switch events

--- packages/termworld/example/integration_test/linux_ibus_e2e_test.dart
+++ packages/termworld/example/integration_test/linux_ibus_e2e_test.dart
@@ -247,7 +247,16 @@ final class _ImeHarness {
     await tester.pumpAndSettle();
   }
 
-  Future<void> toggleLanguage() => keys(<String>['Hangul']);
+  Future<void> toggleLanguage() async {
+    await _releaseModifiers();
+    await _run('xdotool', <String>[
+      'key',
+      '--clearmodifiers',
+      'Hangul',
+    ]);
+    await _releaseModifiers();
+    await tester.pumpAndSettle();
+  }
 
   Future<void> _releaseModifiers() => _run('xdotool', <String>[
     'keyup',
